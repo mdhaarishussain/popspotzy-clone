1867d91c1f15bec3777de536199a6b0f16ec4624
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2025-08-02 11:42:04 +0400

    updated

--- popspotzy-frontend/src/components/sections/CTA.tsx
+++ popspotzy-frontend/src/components/sections/CTA.tsx
@@ -9,7 +9,7 @@ export default function CTA() {
       {/* Background decoration */}
       <div className="absolute inset-0">
         <div className="absolute top-10 left-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-        <div className="absolute top-10 right-10 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div className="absolute top-10 right-10 w-72 h-72 md:bg-yellow-200 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
         <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
       </div>
       
