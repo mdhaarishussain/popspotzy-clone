a452ed9f70175e60a8e67071f70401d8a9f058c2
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2025-08-02 10:34:15 +0400

    updated

--- popspotzy-frontend/src/components/sections/Hero.tsx
+++ popspotzy-frontend/src/components/sections/Hero.tsx
@@ -47,7 +47,7 @@ export default function Hero() {
 
             <div className="flex flex-col sm:flex-row space-y-5 sm:space-y-0 sm:space-x-6 justify-center items-center">
               <GradientButton className="text-md px-5 py-2">
-                Analyze Your Brand &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-sparkles w-5 h-5"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path><path d="M20 3v4"></path><path d="M22 5h-4"></path><path d="M4 17v2"></path><path d="M5 18H3"></path></svg>
+                Analyze Your Brand &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles w-5 h-5"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path><path d="M20 3v4"></path><path d="M22 5h-4"></path><path d="M4 17v2"></path><path d="M5 18H3"></path></svg>
               </GradientButton>
               
               <button className="px-5 py-2 border-2 border-slate-200 text-black rounded-4xl font-medium hover:bg-pink-500 hover:text-white cursor-pointer transition-colors text-md">
